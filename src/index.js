410163b21bc89c35be38acd9676e920d1b122c4b
Ilchenko Daniil Yurievich <crocus161@gmail.com>
2022-01-07 20:05:28 +0200

    Create the base interface for different variant of realisation

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import './index.css';
 import React from 'react';
 import App from './app/App';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {store} from './store/store';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
