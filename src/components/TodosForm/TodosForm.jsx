3806538341f2f1c6fc086cc375957f1940a1f665
Ilchenko Daniil Yurievich <crocus161@gmail.com>
2022-01-13 18:08:38 +0200

    Create error catching in component

--- src/components/TodosForm/TodosForm.jsx
+++ src/components/TodosForm/TodosForm.jsx
@@ -1,27 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addTodo } from '../../store/todo/thunks';
 
 const TodosForm = () => {
-    const dispatch = useDispatch();
-    const [newTodoTitle, setNewTodoTitle] = useState('');
+	const dispatch = useDispatch();
+	const [newTodoTitle, setNewTodoTitle] = useState('');
 
-    const addTodoHandler = (e) => {
-        e.preventDefault();
-        dispatch(addTodo(newTodoTitle));
-        setNewTodoTitle('');
-    }
+	const addTodoHandler = (e) => {
+		e.preventDefault();
+		dispatch(addTodo(newTodoTitle));
+		setNewTodoTitle('');
+	};
 
-    return (
-        <form onSubmit={addTodoHandler}>
-            <input 
-                type="text" 
-                value={newTodoTitle}
-                onChange={e => setNewTodoTitle(e.target.value)}
-            />
-            <button>Добавить</button>
-        </form>
-    )
-}
+	return (
+		<form onSubmit={addTodoHandler}>
+			<input
+				type="text"
+				value={newTodoTitle}
+				onChange={e => setNewTodoTitle(e.target.value)}
+			/>
+			<button>Добавить</button>
+		</form>
+	);
+};
 
-export default TodosForm
+export default TodosForm;
--- src/components/TodosList/TodosList.jsx
+++ src/components/TodosList/TodosList.jsx
@@ -1,26 +1,24 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { setTodos } from '../../store/todo/thunks';
 import TodosItem from './TodosItem';
 
 const TodosList = () => {
-    const dispatch = useDispatch();
-    const { todos, error } = useSelector(state => state.todo);
+	const dispatch = useDispatch();
+	const {todos, error} = useSelector(state => state.todo);
 
-    useEffect(() => {
-        dispatch(setTodos());
-    }, []);
+	useEffect(() => {
+		dispatch(setTodos());
+	}, [dispatch]);
 
-    return (
-        <ul>
-            {todos.map(todo => (
-                <TodosItem
-                    todo={todo}
-                    key={todo.id} 
-                />
-            ))}
-        </ul>
-    )
-}
+	return (
+		<>
+			{error && <h2>Error message</h2>}
+			<ul>
+				{todos.map(todo => <TodosItem todo={todo} key={todo.id}/>)}
+			</ul>
+		</>
+	);
+};
 
-export default TodosList
+export default TodosList;
